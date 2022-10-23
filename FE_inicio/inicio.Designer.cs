67d8b50baceebbc24c70b021f830b2be8bdf40cd
LeanN TS <leonicolas6@gmail.com>
2022-10-23 00:58:54 -0300

    Agregar archivos de proyecto.

---
+++ FE_inicio/inicio.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace FE_inicio
+{
+    partial class inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cPGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nosotrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vEHICULOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aUTOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uSADOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mOTOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cLIENTESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asdToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lISTADECLIENTESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cARGARCLIENTEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mODIFICARCLIENTEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.primerasMarcasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.segundasMarcasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cPGToolStripMenuItem,
+            this.vEHICULOSToolStripMenuItem,
+            this.cLIENTESToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cPGToolStripMenuItem
+            // 
+            this.cPGToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nosotrosToolStripMenuItem});
+            this.cPGToolStripMenuItem.Name = "cPGToolStripMenuItem";
+            this.cPGToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.cPGToolStripMenuItem.Text = "CPG";
+            // 
+            // nosotrosToolStripMenuItem
+            // 
+            this.nosotrosToolStripMenuItem.Name = "nosotrosToolStripMenuItem";
+            this.nosotrosToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.nosotrosToolStripMenuItem.Text = "Nosotros";
+            // 
+            // vEHICULOSToolStripMenuItem
+            // 
+            this.vEHICULOSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aUTOSToolStripMenuItem,
+            this.mOTOSToolStripMenuItem});
+            this.vEHICULOSToolStripMenuItem.Name = "vEHICULOSToolStripMenuItem";
+            this.vEHICULOSToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.vEHICULOSToolStripMenuItem.Text = "VEHICULOS";
+            // 
+            // aUTOSToolStripMenuItem
+            // 
+            this.aUTOSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kMToolStripMenuItem,
+            this.uSADOSToolStripMenuItem});
+            this.aUTOSToolStripMenuItem.Name = "aUTOSToolStripMenuItem";
+            this.aUTOSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aUTOSToolStripMenuItem.Text = "AUTOS";
+            // 
+            // kMToolStripMenuItem
+            // 
+            this.kMToolStripMenuItem.Name = "kMToolStripMenuItem";
+            this.kMToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kMToolStripMenuItem.Text = "0KM";
+            // 
+            // uSADOSToolStripMenuItem
+            // 
+            this.uSADOSToolStripMenuItem.Name = "uSADOSToolStripMenuItem";
+            this.uSADOSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uSADOSToolStripMenuItem.Text = "USADOS";
+            // 
+            // mOTOSToolStripMenuItem
+            // 
+            this.mOTOSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.primerasMarcasToolStripMenuItem,
+            this.segundasMarcasToolStripMenuItem});
+            this.mOTOSToolStripMenuItem.Name = "mOTOSToolStripMenuItem";
+            this.mOTOSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mOTOSToolStripMenuItem.Text = "MOTOS";
+            // 
+            // cLIENTESToolStripMenuItem
+            // 
+            this.cLIENTESToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lISTADECLIENTESToolStripMenuItem,
+            this.cARGARCLIENTEToolStripMenuItem,
+            this.mODIFICARCLIENTEToolStripMenuItem});
+            this.cLIENTESToolStripMenuItem.Name = "cLIENTESToolStripMenuItem";
+            this.cLIENTESToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.cLIENTESToolStripMenuItem.Text = "CLIENTES";
+            // 
+            // asdToolStripMenuItem4
+            // 
+            this.asdToolStripMenuItem4.Name = "asdToolStripMenuItem4";
+            this.asdToolStripMenuItem4.Size = new System.Drawing.Size(180, 22);
+            this.asdToolStripMenuItem4.Text = "Nosotros";
+            // 
+            // lISTADECLIENTESToolStripMenuItem
+            // 
+            this.lISTADECLIENTESToolStripMenuItem.Name = "lISTADECLIENTESToolStripMenuItem";
+            this.lISTADECLIENTESToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.lISTADECLIENTESToolStripMenuItem.Text = "LISTA DE CLIENTES";
+            // 
+            // cARGARCLIENTEToolStripMenuItem
+            // 
+            this.cARGARCLIENTEToolStripMenuItem.Name = "cARGARCLIENTEToolStripMenuItem";
+            this.cARGARCLIENTEToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.cARGARCLIENTEToolStripMenuItem.Text = "CARGAR CLIENTE";
+            // 
+            // mODIFICARCLIENTEToolStripMenuItem
+            // 
+            this.mODIFICARCLIENTEToolStripMenuItem.Name = "mODIFICARCLIENTEToolStripMenuItem";
+            this.mODIFICARCLIENTEToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.mODIFICARCLIENTEToolStripMenuItem.Text = "MODIFICAR CLIENTE";
+            // 
+            // primerasMarcasToolStripMenuItem
+            // 
+            this.primerasMarcasToolStripMenuItem.Name = "primerasMarcasToolStripMenuItem";
+            this.primerasMarcasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.primerasMarcasToolStripMenuItem.Text = "Primeras Marcas";
+            // 
+            // segundasMarcasToolStripMenuItem
+            // 
+            this.segundasMarcasToolStripMenuItem.Name = "segundasMarcasToolStripMenuItem";
+            this.segundasMarcasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.segundasMarcasToolStripMenuItem.Text = "Segundas Marcas";
+            // 
+            // inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "inicio";
+            this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cPGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nosotrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vEHICULOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aUTOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uSADOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mOTOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cLIENTESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asdToolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem primerasMarcasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem segundasMarcasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lISTADECLIENTESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cARGARCLIENTEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mODIFICARCLIENTEToolStripMenuItem;
+    }
+}
+
